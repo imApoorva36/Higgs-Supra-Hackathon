d2c089cb42434cd12cd443ebdd499b855cf9e92b
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2025-01-18 19:59:47 +0530

    Add Tag model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    tag_id = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tag_id
